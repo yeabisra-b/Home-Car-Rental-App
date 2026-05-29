452d3b08dda20a04fdfe40c4931aec3373137729
theluchadora <tsionfelekee@gmail.com>
2026-05-30 01:25:52 +0300

    reorganise the project structure

--- backend/src/tests/globalTeardown.ts
+++
@@ -1,5 +0,0 @@
-import { stopTestPostgresServer } from '../database/testPostgresServer';
-
-export default async function globalTeardown(): Promise<void> {
-  await stopTestPostgresServer().catch(() => undefined);
-}
